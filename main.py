9e37ef3e97e2701ce4696a9ca0b3a075870e1c7f
kokis128 <jorgevalentini76@gmail.com>
2026-04-22 06:43:58 -0300

    modify  router

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI, HTTPException
 
-from router import products,users
+
 
 from pydantic import BaseModel
 from typing import List
+
+from router import products,users
 
 app = FastAPI()
 
 #Routers
 app.include_router(products.router)
 app.include_router(users.router)
+
 
 class User(BaseModel):
     id: int
